0adef3c48879068b585134288311a00d493acf55
ablackcamellia <65071721+ablackcamellia@users.noreply.github.com>
2026-02-02 02:27:40 +0800

    Update config.js

--- themes/movie/config.js
+++ themes/movie/config.js
@@ -11,9 +11,9 @@ const CONFIG = {
 
   MOVIE_PREVIEW_CATEGORY_COUNT: 16, // 首页最多展示的分类数量，0为不限制
   MOVIE_ARTICLE_RECOMMEND: true, // 推荐关联内容在文章底部
-  MOVIE_VIDEO_COMBINE: true, // 聚合视频，开启后一篇文章内的多个含caption的视频会被合并到文章开头，并展示分集按钮
+  MOVIE_VIDEO_COMBINE: false, // 聚合视频，开启后一篇文章内的多个含caption的视频会被合并到文章开头，并展示分集按钮
   MOVIE_VIDEO_COMBINE_SHOW_PAGE_FORCE: false, // 即使只有一集也显示集数切换按钮
 
-  MOVIE_POST_LIST_COVER: true // 列表显示文章封面
+  MOVIE_POST_LIST_COVER: false // 列表显示文章封面
 }
 export default CONFIG
